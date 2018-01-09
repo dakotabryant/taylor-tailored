ce75f1fdb4c17f96687a3bfd405db1bda2f0a8f2
Dakota Bryant <bryant.dakota@gmail.com>
2018-01-09 00:09:17 -0500

    trying to fix build again...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Hello from '../components/hello';
-import About from '../components/about';
-import IconBar from '../components/iconBar';
+import Hello from '../components/hello/index';
+import About from '../components/about/index';
+import IconBar from '../components/iconBar/index';
 import icons from '../assets/iconBar';
 
 const IndexPage = () => (
