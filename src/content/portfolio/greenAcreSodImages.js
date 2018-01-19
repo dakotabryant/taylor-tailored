ce85dd7961c7f51a84b3462ff8de5d6b6b7fa984
Dakota Bryant <bryant.dakota@gmail.com>
2018-01-18 23:09:49 -0500

    more progress, added all photos

---
+++ src/content/portfolio/greenAcreSodImages.js
@@ -0,0 +1,17 @@
+import Logo1 from '../../assets/portfolio/greenAcre/logo-1.jpg';
+import Logo2 from '../../assets/portfolio/greenAcre/logo-2.jpg';
+import FlatBed from '../../assets/portfolio/greenAcre/Flatbed_Mockup-min.jpg';
+import Branding from '../../assets/portfolio/greenAcre/Mockup_Branding2-min.jpg';
+import BusinessCard from '../../assets/portfolio/greenAcre/Mockup_BusinessCard-min.jpg';
+import Swatches from '../../assets/portfolio/greenAcre/swatches.png';
+import Tshirt from '../../assets/portfolio/greenAcre/tshirt-min.jpg';
+
+export default [
+  Logo1,
+  Logo2,
+  FlatBed,
+  Branding,
+  BusinessCard,
+  Swatches,
+  Tshirt,
+];
---
+++ src/content/portfolio/redLobsterImages.js
@@ -0,0 +1,8 @@
+import Logo from '../../assets/portfolio/redLobster/logo.png';
+import Mockup1 from '../../assets/portfolio/redLobster/Mockup1-min.jpg';
+import Mockup2 from '../../assets/portfolio/redLobster/Mockup2-min.jpg';
+import Mockup3 from '../../assets/portfolio/redLobster/Mockup3-min.jpg';
+import Board1 from '../../assets/portfolio/redLobster/board1.jpg';
+import Board2 from '../../assets/portfolio/redLobster/board2.jpg';
+
+export default [Logo, Mockup1, Board1, Mockup2, Mockup3, Board2];
