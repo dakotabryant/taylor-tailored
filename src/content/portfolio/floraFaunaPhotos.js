08c793c72c0e9eccbdab22ab26e86d98716958da
Dakota Bryant <bryant.dakota@gmail.com>
2018-01-22 01:07:37 -0500

    final changes

--- src/content/portfolio/floraFaunaPhotos.js
+++ src/content/portfolio/floraFaunaPhotos.js
@@ -1,8 +1,7 @@
 import flauraAll from '../../assets/portfolio/floraFauna/FloraFauna_All-min.jpg';
 import bathSalts from '../../assets/portfolio/floraFauna/FloraFauna_BathSalts-min.jpg';
-import cleanser from '../../assets/portfolio/floraFauna/FloraFauna_FaceCleanser-min.jpg';
+import cleanser from '../../assets/portfolio/floraFauna/faceCleanser-min.jpg';
 import moisturizer from '../../assets/portfolio/floraFauna/FloraFauna_FaceMoisturizer-min.jpg';
 import creamNew from '../../assets/portfolio/floraFauna/creamNew.jpg';
 
 export default [flauraAll, bathSalts, cleanser, moisturizer, creamNew];
-
