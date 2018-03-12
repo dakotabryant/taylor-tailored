eccc1e112831cb6a46775050c4c25955a3772c66
Dakota Bryant <bryant.dakota@gmail.com>
2018-03-11 23:09:48 -0400

    Revert "added some slide animations"

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -8,34 +8,31 @@ import ButtonGroup from './ButtonGroup';
 import Icon from './Icon';
 import MobileScroll from '../assets/scrollMobile.svg';
 import DesktopScroll from '../assets/scrollDesktop.svg';
-import { Reveal } from 'react-reveal';
 
 const Hello = () => {
-	let mobileWidth;
-	if (typeof window !== 'undefined') {
-		mobileWidth = window.innerWidth <= 990;
-	}
-	const currentIcon = mobileWidth ? MobileScroll : DesktopScroll;
-	return (
-		<Section className="hello" id="home">
-			<Container className="hello__container">
-				<Reveal right>
-					<Text tag="h1" className="uppercase bold hello__text">
-						Hello <span className="lowercase light">i'm taylor</span>
-					</Text>
-					<ButtonGroup>
-						<Link to="#portfolio">
-							<Button className="button uppercase">View My Work</Button>
-						</Link>
-						<a href="mailto:mrstaylorbryant@gmail.com?subject=You blew me away with your site!">
-							<Button className="button uppercase">Contact Me</Button>
-						</a>
-					</ButtonGroup>
-				</Reveal>
-			</Container>
-			<img src={currentIcon} className="scroll-icon" />
-		</Section>
-	);
+  let mobileWidth;
+  if (typeof window !== 'undefined') {
+    mobileWidth = window.innerWidth <= 990;
+  }
+  const currentIcon = mobileWidth ? MobileScroll : DesktopScroll;
+  return (
+    <Section className="hello" id="home">
+      <Container className="hello__container">
+        <Text tag="h1" className="uppercase bold hello__text">
+          Hello <span className="lowercase light">i'm taylor</span>
+        </Text>
+        <ButtonGroup>
+          <Link to="#portfolio">
+            <Button className="button uppercase">View My Work</Button>
+          </Link>
+          <a href="mailto:mrstaylorbryant@gmail.com?subject=You blew me away with your site!">
+            <Button className="button uppercase">Contact Me</Button>
+          </a>
+        </ButtonGroup>
+      </Container>
+      <img src={currentIcon} className="scroll-icon" />
+    </Section>
+  );
 };
 
 export default Hello;
