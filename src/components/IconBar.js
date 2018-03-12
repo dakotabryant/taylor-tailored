bae25ed1662f0899696bd0b23ca738861c39451b
Dakota Bryant <bryant.dakota@gmail.com>
2018-03-11 23:03:53 -0400

    added some slide animations

--- src/components/IconBar.js
+++ src/components/IconBar.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import Section from './Section';
 import Icon from './Icon';
+import { Fade } from 'react-reveal';
 
 const IconBar = props => {
-  const { icons } = props;
-  return (
-    <Section className="icon-bar">
-      <div className="icon-bar__inner-container">
-        {icons &&
-          icons.map(icon => {
-            return (
-              <div key={icon.name} className="icon-bar__text-containers">
-                <img src={icon.path} className="icon-bar__icons" />
-                <p>{icon.name}</p>
-              </div>
-            );
-          })}
-      </div>
-    </Section>
-  );
+	const { icons } = props;
+	return (
+		<Section className="icon-bar">
+			<div className="icon-bar__inner-container">
+				{icons &&
+					icons.map(icon => {
+						return (
+							<Fade left>
+								<div key={icon.name} className="icon-bar__text-containers">
+									<img src={icon.path} className="icon-bar__icons" />
+									<p>{icon.name}</p>
+								</div>
+							</Fade>
+						);
+					})}
+			</div>
+		</Section>
+	);
 };
 
 export default IconBar;
--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -2,26 +2,32 @@ import React from 'react';
 import Container from './Container';
 import Text from './Text';
 import Link from 'gatsby-link';
+import { Fade } from 'react-reveal';
 
 const PortfolioGrid = ({ className, currentTab, portfolioItems, onClick }) => {
-  return (
-    <div className="inner-grid">
-      {portfolioItems &&
-        portfolioItems.map(item => {
-          return (
-            <Link to="/portfolio/" key={item.title}>
-              <Container className="grid-item">
-                <img src={item.image} alt={item.title} />
-                <div className="hover-container" onClick={() => onClick(item)}>
-                  <Text tag="h3">{item.title}</Text>
-                  <div>{item.tags.map(tag => <p key={tag}>{tag}</p>)}</div>
-                </div>
-              </Container>
-            </Link>
-          );
-        })}
-    </div>
-  );
+	return (
+		<div className="inner-grid">
+			{portfolioItems &&
+				portfolioItems.map(item => {
+					return (
+						<Fade left>
+							<Link to="/portfolio/" key={item.title}>
+								<Container className="grid-item">
+									<img src={item.image} alt={item.title} />
+									<div
+										className="hover-container"
+										onClick={() => onClick(item)}
+									>
+										<Text tag="h3">{item.title}</Text>
+										<div>{item.tags.map(tag => <p key={tag}>{tag}</p>)}</div>
+									</div>
+								</Container>
+							</Link>
+						</Fade>
+					);
+				})}
+		</div>
+	);
 };
 
 export default PortfolioGrid;
