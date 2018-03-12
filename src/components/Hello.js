27ac9a72d07a05cfb56a1b2333d3d01f81e11be1
Dakota Bryant <bryant.dakota@gmail.com>
2018-03-11 22:45:54 -0400

    can't get this trash to work how I want it. I'll come back later

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -8,31 +8,40 @@ import ButtonGroup from './ButtonGroup';
 import Icon from './Icon';
 import MobileScroll from '../assets/scrollMobile.svg';
 import DesktopScroll from '../assets/scrollDesktop.svg';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Hello = () => {
-  let mobileWidth;
-  if (typeof window !== 'undefined') {
-    mobileWidth = window.innerWidth <= 990;
-  }
-  const currentIcon = mobileWidth ? MobileScroll : DesktopScroll;
-  return (
-    <Section className="hello" id="home">
-      <Container className="hello__container">
-        <Text tag="h1" className="uppercase bold hello__text">
-          Hello <span className="lowercase light">i'm taylor</span>
-        </Text>
-        <ButtonGroup>
-          <Link to="#portfolio">
-            <Button className="button uppercase">View My Work</Button>
-          </Link>
-          <a href="mailto:mrstaylorbryant@gmail.com?subject=You blew me away with your site!">
-            <Button className="button uppercase">Contact Me</Button>
-          </a>
-        </ButtonGroup>
-      </Container>
-      <img src={currentIcon} className="scroll-icon" />
-    </Section>
-  );
+	let mobileWidth;
+	if (typeof window !== 'undefined') {
+		mobileWidth = window.innerWidth <= 990;
+	}
+	const currentIcon = mobileWidth ? MobileScroll : DesktopScroll;
+	return (
+		<Section className="hello" id="home">
+			<Container className="hello__container">
+				<ReactCSSTransitionGroup
+					transitionName="hello"
+					transitionAppear={true}
+					transitionAppearTimeout={2000}
+					transitionEnter={false}
+					transitionLeave={false}
+				>
+					<Text tag="h1" className="uppercase bold hello__text">
+						Hello <span className="lowercase light">i'm taylor</span>
+					</Text>
+				</ReactCSSTransitionGroup>
+				<ButtonGroup>
+					<Link to="#portfolio">
+						<Button className="button uppercase">View My Work</Button>
+					</Link>
+					<a href="mailto:mrstaylorbryant@gmail.com?subject=You blew me away with your site!">
+						<Button className="button uppercase">Contact Me</Button>
+					</a>
+				</ButtonGroup>
+			</Container>
+			<img src={currentIcon} className="scroll-icon" />
+		</Section>
+	);
 };
 
 export default Hello;
