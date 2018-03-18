870eff0375d1e6273db238dc753f0adebb5ef289
Dakota Bryant <bryant.dakota@gmail.com>
2018-03-18 16:53:20 -0400

    about page fixes

--- src/content/about/experienceText.js
+++ src/content/about/experienceText.js
@@ -1,31 +1,31 @@
 export default [
-  {
-    jobTitle: 'LEAD DESIGNER',
-    company: 'Grit Media | Feb. 2018 - Present'
-  }
-  {
-    jobTitle: 'WEB DESIGNER / CONTENT MANAGER',
-    company: 'Collipsis Web Solutions | Aug. 2014 - Mar. 2018',
-  },
-  {
-    jobTitle: 'CREATIVE SERVICES MANAGER',
-    company: 'Littlefield Agency | Jan. 2016 - Nov. 2017',
-  },
-  {
-    jobTitle: 'MARKETING ASSISTANT',
-    company: 'Mabee Center | Aug. 2015 - Dec. 2015',
-  },
-  {
-    jobTitle: 'COMMUNICATIONS INTERN',
-    company:
-      'New Hampshire | Automobile Dealers Association May-Aug. 2013 & 2014',
-  },
-  {
-    jobTitle: 'CREATIVE INTERN',
-    company: 'BlueSpeed AV | Jan. 2014 - May 2014',
-  },
-  {
-    jobTitle: 'GRAPHIC DESIGN COORDINATOR',
-    company: 'Oral Roberts University | Sept. 2013 - May 2014',
-  },
+	{
+		jobTitle: 'LEAD DESIGNER',
+		company: 'Grit Media | Feb. 2018 - Present',
+	},
+	{
+		jobTitle: 'WEB DESIGNER / CONTENT MANAGER',
+		company: 'Collipsis Web Solutions | Aug. 2014 - Mar. 2018',
+	},
+	{
+		jobTitle: 'CREATIVE SERVICES MANAGER',
+		company: 'Littlefield Agency | Jan. 2016 - Nov. 2017',
+	},
+	{
+		jobTitle: 'MARKETING ASSISTANT',
+		company: 'Mabee Center | Aug. 2015 - Dec. 2015',
+	},
+	{
+		jobTitle: 'COMMUNICATIONS INTERN',
+		company:
+			'New Hampshire | Automobile Dealers Association May-Aug. 2013 & 2014',
+	},
+	{
+		jobTitle: 'CREATIVE INTERN',
+		company: 'BlueSpeed AV | Jan. 2014 - May 2014',
+	},
+	{
+		jobTitle: 'GRAPHIC DESIGN COORDINATOR',
+		company: 'Oral Roberts University | Sept. 2013 - May 2014',
+	},
 ];
