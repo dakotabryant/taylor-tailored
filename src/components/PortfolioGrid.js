a999f358c9ed409d164f5b7c8d2467de2d92e6ce
Dakota Bryant <bryant.dakota@gmail.com>
2018-03-18 16:35:22 -0400

    last animation

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from './Container';
 import Text from './Text';
 import Link from 'gatsby-link';
+import { Fade } from 'react-reveal';
 
 const PortfolioGrid = ({ className, currentTab, portfolioItems, onClick }) => {
 	return (
@@ -9,15 +10,20 @@ const PortfolioGrid = ({ className, currentTab, portfolioItems, onClick }) => {
 			{portfolioItems &&
 				portfolioItems.map(item => {
 					return (
-						<Link to="/portfolio/" key={item.title}>
-							<Container className="grid-item">
-								<img src={item.image} alt={item.title} />
-								<div className="hover-container" onClick={() => onClick(item)}>
-									<Text tag="h3">{item.title}</Text>
-									<div>{item.tags.map(tag => <p key={tag}>{tag}</p>)}</div>
-								</div>
-							</Container>
-						</Link>
+						<Fade left>
+							<Link to="/portfolio/" key={item.title}>
+								<Container className="grid-item">
+									<img src={item.image} alt={item.title} />
+									<div
+										className="hover-container"
+										onClick={() => onClick(item)}
+									>
+										<Text tag="h3">{item.title}</Text>
+										<div>{item.tags.map(tag => <p key={tag}>{tag}</p>)}</div>
+									</div>
+								</Container>
+							</Link>
+						</Fade>
 					);
 				})}
 		</div>
